4e24df1005544296ac6c11ebb9c1342338b7d612
Jeff Brunson <jeff.a.brunson@gmail.com>
2015-11-09 11:50:38 -0700

    add signin form

--- simple-todos.js
+++ simple-todos.js
@@ -26,7 +26,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -45,6 +47,10 @@ if (Meteor.isClient) {
     "click .delete": function() {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
